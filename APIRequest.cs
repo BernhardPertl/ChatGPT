2a42c9159beaf3a5b7d74d70005d814fdbf87b85
Bernhard Pertl <5345185+BernhardPertl@users.noreply.github.com>
2023-02-06 11:08:19 +0100

    Add files via upload

---
+++ APIRequest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOpenAIChat
+{
+    public class APIRequest
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; } = "text-davinci-003";
+
+        [JsonPropertyName("prompt")]
+        public string Question { get; set; } = "";
+
+        [JsonPropertyName("max_tokens")]
+        public int MaxTokens { get; set; } = 4000;
+
+        [JsonPropertyName("temperature")]
+        public float Accuracy { get; set; } = 0.6F;    // 0 = very accurate, 1 = not very accurate
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+    public class APIResponse
+    {
+        [JsonPropertyName("choices")]
+        public List<Answer>? Answers { get; set; }
+
+        public override string ToString()
+        {
+            string answer = string.Empty;
+
+            if (Answers != null && Answers.Any())
+            {
+                answer = string.Join(Environment.NewLine, Answers.Select(s => s.Text.Replace("\n\n", "\n")));
+                if (answer.StartsWith("\n"))
+                {
+                    return answer.Substring(1);
+                }
+            }
+
+            return answer;
+        }
+
+        public class Answer
+        {
+            [JsonPropertyName("text")]
+            public string Text { get; set; } = string.Empty;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Text.Json;
+
+using ConsoleAppOpenAIChat;
+
+internal class Program
+{
+    private const string API_KEY = "YOUR_API_KEY";
+
+    private static void Main(string[] args)
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization",$"Bearer {API_KEY}");
+
+        Console.WriteLine("ChatGPT console");
+
+        string line = string.Empty;
+        while (Continue(line))
+        {
+            line = GetQuestion();
+            if (!string.IsNullOrEmpty(line) && Continue(line))
+            {
+                var response = client.PostAsync("https://api.openai.com/v1/completions",
+                               new StringContent(new APIRequest(){ Question = line }.ToJson(), Encoding.UTF8, "application/json")).Result;
+
+                ShowAnswer(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+
+    private static bool Continue(string line)
+    {
+        if (line.Equals("e", StringComparison.OrdinalIgnoreCase) ||
+            line.Equals("end", StringComparison.OrdinalIgnoreCase) || 
+            line.Equals("exit", StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static string GetQuestion()
+    {
+        Console.Write("Question: ");
+        return Console.ReadLine() + string.Empty;
+    }
+
+    private static void ShowAnswer(string responseData)
+    {
+        var response = JsonSerializer.Deserialize<APIResponse>(responseData);
+        if (response != null)
+        {
+            Console.WriteLine("Answer:   " + response.ToString());
+        }
+    }
+}
